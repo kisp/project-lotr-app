47696224408fb31ec2821f6d14e4c5c4aa90768b
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-21 14:57:03 +0200

    Fix build error by providing a default "volume" object

--- pages/volumes/[slug].js
+++ pages/volumes/[slug].js
@@ -43,7 +43,11 @@ const Volume = () => {
     query: { slug },
   } = useRouter();
 
-  const volume = findVolume(slug);
+  const volume = findVolume(slug) || {
+    title: "untitled",
+    description: "no description",
+    books: [],
+  };
 
   const { title, description, books } = volume;
 
